6ec967d85fd631040fed6a9951835d2b41923325
Joel Bradshaw <cincodenada@gmail.com>
2013-07-13 17:36:25 -0700

    Adding two-player support to NES code

--- nes/neskeys.c
+++ nes/neskeys.c
@@ -34,16 +34,20 @@
 #define LED_OFF		(PORTD |= (1<<6))
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 #define SLOW 1
+#define NUMP 2
 
-uint8_t number_keys[10]=
-	{KEY_0,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9};
-
-uint8_t key_names[8] =
-    {'A','B','S','s','^','v','<','>'};
-uint8_t key_press[8] =
-    {KEY_X,KEY_Z,KEY_ENTER,KEY_CTRL,KEY_UP,KEY_DOWN,KEY_LEFT,KEY_RIGHT};
-uint8_t is_modifier[8] =
-    {0,0,0,1,0,0,0,0};
+uint8_t key_names[2][8] = {
+    {'A','B','S','S','^','v','<','>'},
+    {' ',' ','e','t',' ',' ',' ',' '}
+};
+uint8_t key_press[NUMP][8] = {
+    {KEY_X,KEY_Z,KEY_ENTER,KEY_CTRL,KEY_UP,KEY_DOWN,KEY_LEFT,KEY_RIGHT},
+    {KEY_ALT,KEY_SHIFT,KEY_Q,KEY_E,KEY_W,KEY_S,KEY_A,KEY_D}
+};
+uint8_t is_modifier[NUMP][8] = {
+    {0,0,0,1,0,0,0,0},
+    {1,1,0,0,0,0,0,0}
+};
 
 uint16_t idle_count=0;
 
@@ -55,7 +59,9 @@ int main(void)
 	// See the "Using I/O Pins" page for details.
 	// http://www.pjrc.com/teensy/pins.html
 	DDRC = 0x06;    // 00000110 - pins 1 & 2 output, pin 3 (etc) input
+	DDRF = 0x06;    // 00000110 - pins 1 & 2 output, pin 3 (etc) input
 	PORTC = 0xF9;   // 11111001 pins 1 & 2 low to begin, everything else pullup
+	PORTF = 0xF9;   // 11111001 pins 1 & 2 low to begin, everything else pullup
 
 	// Initialize the USB, and then wait for the host to set configuration.
 	// If the Teensy is powered without a PC connected to the USB port,
@@ -81,71 +87,94 @@ int main(void)
     }
 }
 
+uint8_t poll_controller(volatile uint8_t *port, volatile uint8_t *pin) {
+    uint8_t resp = 0;
+    uint8_t curbit;
+
+    //Send a 12-us pulse to the latch pin
+    *port |= (1<<2);
+    _delay_us(12*SLOW);
+    *port &= ~(1<<2);
+    //Record A
+    if (!(*pin & (1<<3))) {    //Check for ground-assertion on input pin
+        resp |= 1;
+    }
+
+    //Wait six us, then send 8x 12-us (50% duty cycle) pulses and record input
+    curbit = 1;
+    while(curbit < 8) {
+        _delay_us(6*SLOW);
+        *port |= (1<<1);            //Send 6ms pulse
+        _delay_us(6*SLOW);
+        *port &= ~(1<<1);           //Reset to ground
+        if (!(*pin & (1<<3))) {    //Check for ground-assertion on input pin
+            resp |= 1 << curbit;
+        }
+        curbit++;
+    }
+
+    return resp;
+}
+
 // This interrupt routine is run approx 61 times per second.
 // A very simple inactivity timeout is implemented, where we
 // will send a space character and print a message to the
 // hid_listen debug message window.
 ISR(TIMER0_OVF_vect)
 {
-        //Holds the button states.  A, B, Select, Start, Up, Down, Left, Right
-        uint8_t resp;
-        uint8_t curbit, curkey;
-        resp = 0;
+    int resp[NUMP];
+    resp[0] = poll_controller(&PORTC, &PINC);
+    resp[1] = poll_controller(&PORTF, &PINF);
 
-        //Send a 12-us pulse to the latch pin
-        PORTC |= (1<<2);
-        _delay_us(12*SLOW);
-        PORTC &= ~(1<<2);
-        //Record A
-        if (!(PINC & (1<<3))) {    //Check for ground-assertion on input pin
-            resp |= 1;
-        }
+    print("Buttons pressed:\n");
+    print("P1 P2 P3 P4\n");
 
-        //Wait six us, then send 8x 12-us (50% duty cycle) pulses and record input
-        curbit = 1;
-        while(curbit < 8) {
-            _delay_us(6*SLOW);
-            PORTC |= (1<<1);            //Send 6ms pulse
-            _delay_us(6*SLOW);
-            PORTC &= ~(1<<1);           //Reset to ground
-            if (!(PINC & (1<<3))) {    //Check for ground-assertion on input pin
-                resp |= 1 << curbit;
-            }
-            curbit++;
-        }
+    //Reset the USB keyboard registers
+    memset(keyboard_keys, 0, 6);
+    keyboard_modifier_keys = 0;
 
-        print("Buttons pressed: ");
-
-        //Reset the USB keyboard registers
-        memset(keyboard_keys, 0, 6);
-        keyboard_modifier_keys = 0;
-
-        //Initialize the counters
-        curbit = 0;
-        curkey = 0;
-        while(curbit < 8) {
+    //Initialize the counters
+    int curbit, curkey, curp;
+    int hadkey;
+    curkey = 0;
+    for(curbit = 0; curbit < 8; curbit++) {
+        hadkey = 0;
+        for(curp = 0; curp < NUMP; curp++) {
             //Check the current bit
-            if(resp & (1 << curbit)) {
-                pchar(key_names[curbit]);
+            if(resp[curp] & (1 << curbit)) {
+                hadkey = 1;
+                pchar(key_names[0][curbit]);
+                pchar(key_names[1][curbit]);
+                pchar(' ');
                 //Set the bits in the proper keyboard register
-                if(is_modifier[curbit]) {
-                    keyboard_modifier_keys |= key_press[curbit];
+                if(is_modifier[curp][curbit]) {
+                    keyboard_modifier_keys |= key_press[curp][curbit];
                 } else {
                     if(curkey < 6) {
-                        keyboard_keys[curkey] = key_press[curbit];
+                        keyboard_keys[curkey] = key_press[curp][curbit];
                         curkey++;
                     } else {
                         //If we overflowed the 6 keys at once, send an error
-                        print("/!\\");
+                        pchar(8);   //Backspace
+                        pchar('!');
+                        pchar(7);   //Bell
                     }
                 }
+            } else {
+                print("   ");
             }
-            curbit++;
         }
+        if(hadkey) {
+            pchar('\n');
+        } else {
+            for(int i=0; i < NUMP*3; i++) {
+                pchar(8);
+            }
+        }
+    }
 
-        //Update the keyboard state
-        usb_keyboard_send();
-        print("\n");
+    //Update the keyboard state
+    usb_keyboard_send();
 }
 
 
