d679d54347ce2bfb8f70f45a56846925f90c98a9
Joel Bradshaw <cincodenada@gmail.com>
2020-01-10 00:05:22 -0800

    Fixes for SimpleString
    "I'll just write my own String class", I said...

--- teensyduino/ControllerPro/simple_string.h
+++ teensyduino/ControllerPro/simple_string.h
@@ -7,8 +7,12 @@ class SimpleString : public std::vector<char> {
  public:
   using Base = std::vector<char>;
   // Copies string into vector
+  SimpleString() : Base() {}
   SimpleString(const char* orig) : Base() {
-    while(orig != '\0') { push_back(*orig); }
+    while(*orig != '\0') {
+      push_back(*orig);
+      orig++;
+    }
   }
   SimpleString(SimpleString&& other) : Base() {
     swap(other);
